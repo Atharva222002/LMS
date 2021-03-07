1722d199ff40cdae16cf6e6788a1df94b908b2dc
Atharva222002 <67674149+Atharva222002@users.noreply.github.com>
2021-03-07 16:46:35 +0530

    Update opco2.js

--- models/opco2.js
+++ models/opco2.js
@@ -8,6 +8,27 @@ var opco2Schema = mongoose.Schema({
   password:String,
   username:String
 });
+
+var bcryptjs = require('bcryptjs')
+opco2Schema.pre('save', function (next) {
+  this.password = bcryptjs.hashSync(this.password, 10);
+  next();
+});
+
+opco2Schema.statics.login = async function(username, password) {
+  const user = await this.findOne({ "username" :  username });
+  if (user) {
+    const auth = await bcryptjs.compareSync(password , user.password)
+    if (auth) {
+      return user;
+    }
+    throw Error('Incorrect password');
+  }
+  throw Error('Incorrect email');
+};
+
+
+
 opco2Schema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model("opco2", opco2Schema);
+module.exports = mongoose.model("OpCo2", opco2Schema);
