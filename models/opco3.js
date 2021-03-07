4046c3032d8519eb3019ed51346ac1e9ae9731c9
Atharva222002 <67674149+Atharva222002@users.noreply.github.com>
2021-03-07 16:47:01 +0530

    Update opco3.js

--- models/opco3.js
+++ models/opco3.js
@@ -8,6 +8,27 @@ var opco3Schema = mongoose.Schema({
   password:String,
   username:String
 });
+
+var bcryptjs = require('bcryptjs')
+opco3Schema.pre('save', function (next) {
+  this.password = bcryptjs.hashSync(this.password, 10);
+  next();
+});
+
+opco3Schema.statics.login = async function(username, password) {
+  const user = await this.findOne({ "username" :  username });
+  if (user) {
+    const auth = await bcryptjs.compareSync(password , user.password)
+    if (auth) {
+      return user;
+    }
+    throw Error('Incorrect password');
+  }
+  throw Error('Incorrect email');
+};
+
+
+
 opco3Schema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model("opco3", opco3Schema);
+module.exports = mongoose.model("OpCo3", opco3Schema);
